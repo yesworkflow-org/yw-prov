5707f4bad6f5105df2e329b22b30e92b7822928d
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-01-04 12:32:52 -0800

    Added model query 4 for simulate_data_collection.

--- src/test/resources/simulate_data_collection/run_queries.sh
+++ src/test/resources/simulate_data_collection/run_queries.sh
@@ -7,3 +7,16 @@ echo 'mq2: What is the name and description of the top-level workflow?'
 arq --data wf_model.ttl                                                     \
     --query mq2.rq                                                          \
     --base http://yesworkflow.org/0000000000/simulate_data_collection/
+    
+#echo
+#echo 'mq3:What are the names of any top-level functions?'
+#arq --data wf_model.ttl                                                     \
+#    --query mq3.rq                                                          \
+#    --base http://yesworkflow.org/0000000000/simulate_data_collection/
+    
+echo
+echo 'What are the names of the programs comprising the top-level workflow?'
+arq --data wf_model.ttl                                                     \
+    --query mq4.rq                                                          \
+    --base http://yesworkflow.org/0000000000/simulate_data_collection/
+    
