7bfbc7a7efb6da752acabec837b47d40159f969f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-01-04 23:54:32 -0800

    Added model query 5.

--- src/test/resources/simulate_data_collection/run_queries.sh
+++ src/test/resources/simulate_data_collection/run_queries.sh
@@ -20,3 +20,8 @@ arq --data wf_model.ttl                                                     \
     --query mq4.rq                                                          \
     --base http://yesworkflow.org/0000000000/simulate_data_collection/
     
+echo
+echo 'mq5: What are the names and descriptions of the inputs to the top-level workflow?'
+arq --data wf_model.ttl                                                     \
+    --query mq5.rq                                                          \
+    --base http://yesworkflow.org/0000000000/simulate_data_collection/
